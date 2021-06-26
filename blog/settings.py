925fbbf0658e9924307530312058f0db81d5cfc2
Sujal Bhakare <sujaldeshmukh1012@gmail.com>
2021-06-26 09:09:30 +0530

    apps interchanging

--- blog/settings.py
+++ blog/settings.py
@@ -25,11 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post',
     'djrichtextfield',
     'rest_framework',
     'corsheaders',
-    'views'
+    'views',
+    'post',
+
 ]
 
 MIDDLEWARE = [
